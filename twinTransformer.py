ff7eea1b79984b74312840dc427957ac0a2064e4
pongmorrakot <pongmorrakot@wisc.edu>
2020-10-15 15:24:56 -0500

    implementing twin transformer

--- twinTransformer.py
+++ twinTransformer.py
@@ -1,2 +1,31 @@
+import torch
+from torch import nn
+
 import transformer
 
+
+class twinTransformer(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        # pdist = nn.PairwiseDistance()
+        euclidean_distance = torch.cdist(output1, output2)
+        # perform contrastive loss calculation with the distance
+        loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) + (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+        return loss_contrastive
